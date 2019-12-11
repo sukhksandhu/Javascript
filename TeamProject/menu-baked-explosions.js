cb9cd82b12b239fba6e0c1cb613127d9db075862
sukhdeep <sukhksandhu@gmail.com>
2019-12-11 18:25:52 -0500

    menu page

--- TeamProject/menu-baked-explosions.js
+++ TeamProject/menu-baked-explosions.js
@@ -1,20 +1,135 @@
-window.onload = function()
-{
-    var slideIndex = 0;
-    showSlides();
+jQuery(document).ready(function(){
+// cakes section javascript
+    $('#list-cake li,#list-donuts li').hover(
+		function(){$(this).css('background','white');},
+		function(){$(this).css('background','#D0ECC5');}
+			
+    );
+    $('#list-cupcakes li,#list-macarons li').hover(
+      function(){$(this).css('background','white');},
+      function(){$(this).css('background','#FFAAA5');}
+        
+      );
+      
+     
+    $("#cake1").hover(
+    function()
+     {
+        $("#img-cake").attr("src","images/cake.jpg");
+      },
+      function(){
+        $("#img-cake").attr("src","images/defaultcake.jpg");
+      });
+    $("#cake2").hover(
+      function()
+        {
+          $("#img-cake").attr("src","images/blackforest.jpg");
+        },
+        function(){
+          $("#img-cake").attr("src","images/defaultcake.jpg");
+        });
+      $("#cake3").hover(
+        function()
+          {
+            $("#img-cake").attr("src","images/strawberry.jpg");
+          },
+          function(){
+            $("#img-cake").attr("src","images/defaultcake.jpg");
+          });
+        $("#cake4").hover(
+        function()
+        {
+          $("#img-cake").attr("src","images/carrot.jpg");
+        },
+        function(){
+          $("#img-cake").attr("src","images/defaultcake.jpg");
+        });
 
-    function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("Slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) 
-    {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
-    }
-                   
-    
-};
+        
+        //section macarons 
+          $("#macarons1").hover(
+          function()
+            {
+              $("#img-macarons").attr("src","images/matcha.jpg");
+            },
+            function(){
+              $("#img-macarons").attr("src","images/macarons.jpg");
+            });
+          $("#macarons2").hover(
+            function()
+              {
+                $("#img-macarons").attr("src","images/raspberry.jpg");
+              },
+              function(){
+                $("#img-macarons").attr("src","images/macarons.jpg");
+              });
+            $("#macarons3").hover(
+              function()
+                {
+                  $("#img-macarons").attr("src","images/mango.jpg").width(400);
+                  // $("#img-macarons").width(400);
+                  //here i am changing the width also of mango macarons image because that was looking odd before
+                },
+                function(){
+                  $("#img-macarons").attr("src","images/macarons.jpg").width(300);
+                });
+          //section donuts
+          $("#donut1").hover(
+            function()
+              {
+                $("#img-donuts").attr("src","images/vanilla.jpg");
+              },
+              function(){
+                $("#img-donuts").attr("src","images/donuts.jpg");
+              });
+            
+          $("#donut2").hover(
+            function()
+              {
+                $("#img-donuts").attr("src","images/coconut.jpg").width(400);
+              },
+              function(){
+                $("#img-donuts").attr("src","images/donuts.jpg").width(300);
+              });
+          $("#donut3").hover(
+            function()
+              {
+                $("#img-donuts").attr("src","images/churro.jpg").css("border-radius","30%");
+              },
+              function(){
+                $("#img-donuts").attr("src","images/donuts.jpg").css("border-radius","50%");
+              });
+//section cupcakes
+          $("#cup1").hover(
+            function()
+              {
+                $("#img-cupcakes").attr("src","images/redvelvet.jpg");
+              },
+              function(){
+                $("#img-cupcakes").attr("src","images/cupcake.jpg");
+              });
+          $("#cup2").hover(
+            function()
+              {
+                $("#img-cupcakes").attr("src","images/coffee.jpg").css("border-radius","30%");
+              },
+              function(){
+                $("#img-cupcakes").attr("src","images/cupcake.jpg").css("border-radius","50%");
+              });
+          $("#cup3").hover(
+            function()
+              {
+                $("#img-cupcakes").attr("src","images/blueberry.jpg").css("border-radius","20%");
+              },
+              function(){
+                $("#img-cupcakes").attr("src","images/cupcake.jpg").css("border-radius","50%");
+              });
+
+
+
+
+
+
+
+
+});
